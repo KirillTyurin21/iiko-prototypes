de46d4e0aa70e5878b1b8463d3eb281700257d73
KirillTyurin21 <KirillTyurin21@users.noreply.github.com>
2026-04-24 13:50:50 +0400

    chore(web-screens): changelog 1.10 released

--- src/app/prototypes/web-screens/changelog.data.ts
+++ src/app/prototypes/web-screens/changelog.data.ts
@@ -4,7 +4,7 @@ export const CHANGELOG: ChangelogRelease[] = [
   {
     version: '1.10',
     date: '2026-04-24',
-    status: 'unreleased',
+    status: 'released',
     changes: [
       {
         page: 'Редактор контролов (Электронная очередь)',
